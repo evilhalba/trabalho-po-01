eff471b12f1fa40a5f33fc8f68c783c11d48649b
Eduardo Cabezudo Vilhalba <vilhalba.20000@hotmail.com>
2019-08-23 21:40:07 -0300

    Update Cliente.java

--- Projeto/Cliente.java
+++ Projeto/Cliente.java
@@ -1,9 +1,42 @@
 public class Cliente{
 	
-	String nome;
-	String CPF;
-	String CNH;
-	String end;
-	String tel;
+	private String nome;
+	private String CPF;
+	private String CNH;
+	private String end;
+	private String tel;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCPF() {
+		return CPF;
+	}
+	public void setCPF(String cPF) {
+		CPF = cPF;
+	}
+	public String getCNH() {
+		return CNH;
+	}
+	public void setCNH(String cNH) {
+		CNH = cNH;
+	}
+	public String getEnd() {
+		return end;
+	}
+	public void setEnd(String end) {
+		this.end = end;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	
+	
 	
 }
