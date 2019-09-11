749d84c7f157b9382d161dccf67d8f469497552d
Eduardo Cabezudo Vilhalba <vilhalba.20000@hotmail.com>
2019-09-11 19:41:18 -0300

    q

---
+++ Projeto/Projeto Projeto/Cliente.java
@@ -0,0 +1,53 @@
+public class Cliente{
+	
+	private int id;
+	private String nome;
+	private String CPF;
+	private String CNH;
+	private String end;
+	private String tel;
+	
+	
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return this.nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCPF() {
+		return CPF;
+	}
+	public void setCPF(String cPF) {
+		CPF = cPF;
+	}
+	public String getCNH() {
+		return CNH;
+	}
+	public void setCNH(String cNH) {
+		CNH = cNH;
+	}
+	public String getEnd() {
+		return end;
+	}
+	public void setEnd(String end) {
+		this.end = end;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	
+	
+	
+}
